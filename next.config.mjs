cd2f61245aa5f4181a5ad2dc3f2f19b123a74c48
Thomas Musselman <tmusselman@gmail.com>
2024-06-19 22:35:33 -0600

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     distDir: 'build',
-    assetPrefix: isProd ? 'https://apps.montanafreepress.org/election-guide-2024' : undefined,
+    assetPrefix: isProd ? 'https://projects.wyofile.com/election-guide-2024' : undefined,
     basePath: '/election-guide-2024',
     trailingSlash: true,
     compiler: {
@@ -15,7 +15,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'apps.montanafreepress.org',
+                hostname: 'projects.wyofile.com',
                 port: '',
                 pathname: '/maps/legislative-districts/**',
             }
