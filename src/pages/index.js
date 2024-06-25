d4acdacf04bfe8de2e73a7f6c943e3dc91766143
Thomas Musselman <tmusselman@gmail.com>
2024-06-24 19:12:03 -0600

    switch to pages router

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export async function getStaticProps() {
+  return {
+    props: {
+      
+    }
+  }
+}
+
+export default function Home() {
+  return (
+    <h1>WyoFile Eleciton Guide</h1>
+  )
+}
