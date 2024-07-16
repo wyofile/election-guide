17dd0a7a191890fdb9f4b38d3299f8f308c6ee9e
Tom Musselman <tmusselm@uwyo.edu>
2024-07-16 13:57:22 -0600

    fix google font import

--- src/design/Layout.js
+++ src/design/Layout.js
@@ -5,7 +5,6 @@ import Head from 'next/head';
 import Header from './Header'
 import Nav from './Nav'
 import Footer from './Footer'
-import Script from 'next/script'
 
 import { metaData } from "@/config";
 
@@ -52,14 +51,6 @@ export default function Layout({
         {/* <meta name="twitter:image" content={featureImage} /> */}
         <meta name="twitter:description" content={socialDescription} />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet" />
-
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
       <Header />
       <Nav />
