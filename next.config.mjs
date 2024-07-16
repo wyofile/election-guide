9b26753e29ef269ee81b3a1d2ea9fd997389cb69
Tom Musselman <tmusselm@uwyo.edu>
2024-07-16 17:48:38 -0600

    fix deployment paths

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     output: 'export',
     distDir: 'build',
-    // assetPrefix: isProd ? 'https://d3lx8jcxdlqvo0.cloudfront.net/election-guide-2024/' : undefined,
+    assetPrefix: isProd ? 'https://projects.wyofile.com/election-guide-2024/' : undefined,
     basePath: '/election-guide-2024',
     trailingSlash: true,
     compiler: {
--- src/config.js
+++ src/config.js
@@ -5,9 +5,8 @@ export const metaData = {
   baseUrl: 'https://projects.wyofile.com/election-guide-2024',
   // On-page display
   // webTitle: '2024 Election Guide', // Replacing with custom design in Header
-  webSubtitle: 'Wyoming\'s candidates for state and federal office.',
+  webSubtitle: 'Wyoming\'s candidates for federal and legislative offices.',
 
   // SEO
   siteSeoTitle: '2024 WyoFile Election Guide'
-
 }
