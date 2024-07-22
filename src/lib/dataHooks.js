26843083dd5e8b36ffc9cccb95d09100c9ec1e7f
Tom Musselman <tmusselman@gmail.com>
2024-07-22 16:49:01 -0600

    change to exclude opinion categories

--- src/lib/dataHooks.js
+++ src/lib/dataHooks.js
@@ -3,13 +3,13 @@ import useSWRImmutable from 'swr/immutable'
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const API_PATH = 'https://wyofile.com/wp-json/wp/v2'
-const OPINION_TAG_ID = 9269
+const OPINION_CATEGORY_ID = 9251
 const JAN_01_2023 = '2023-01-01T00:00:00Z'
 
 export const useStories = (tagId, count) => {
   const fieldsString = ['id', 'date', 'link', 'title'].join(',')
 
-  const key = `${API_PATH}/posts?tags=${tagId}&per_page=${count}&tags_exlude=${OPINION_TAG_ID}&after=${JAN_01_2023}&_fields=${fieldsString}`
+  const key = `${API_PATH}/posts?tags=${tagId}&per_page=${count}&categories_exlude=${OPINION_CATEGORY_ID}&after=${JAN_01_2023}&_fields=${fieldsString}`
 
   const {data: stories, isLoading, error} = useSWRImmutable(key, fetcher)
 
