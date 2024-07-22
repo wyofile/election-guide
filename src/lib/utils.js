00bab70eb31edefe16548596bdf188865d703f7e
Thomas Musselman <tmusselman@gmail.com>
2024-07-21 23:24:47 -0600

    remove unused code

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,26 +1,12 @@
-import { format } from 'd3-format'
 import { timeFormat } from 'd3-time-format'
-
-export const urlize = str => str.toLowerCase().replaceAll(/\s/g, '-')
 
 export const formatDate = timeFormat('%b %-d, %Y')
 export const formatTimeLong = timeFormat('%-I:%M %p %b %-d, %Y')
-
 
 export const pluralize = (text, value) => value === 1 ? text : `${text}s`
 
 export const getDistrictNumber = (key) => {
     return +key.replace('-', '').replace('SD', '').replace('HD', '')
-}
-
-export const dollarFormatResponsive = num => {
-    if (Math.abs(num) < 1000) return format('$,.0f')(num)
-    if (Math.abs(num) >= 1000 && (Math.abs(num) < 10_000)) return format('$,.1s')(num)
-    if (Math.abs(num) >= 10_000 && (Math.abs(num) < 100_000)) return format('$,.2s')(num)
-    if (Math.abs(num) >= 100_000 && (Math.abs(num) < 1_000_000)) return format('$,.3s')(num)
-    if (Math.abs(num) >= 1_000_000 && (Math.abs(num) < 10_000_000)) return format('$,.2s')(num)
-    if (Math.abs(num) >= 10_000_000 && (Math.abs(num) < 100_000_000)) return format('$,.3s')(num)
-    else return 'ERR'
 }
 
 export const formatRace = district => {
