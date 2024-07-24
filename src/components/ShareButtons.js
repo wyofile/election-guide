b109e291b537a503fc3ef2a63e5460ab8e447d5e
Tom Musselman <tmusselman@gmail.com>
2024-07-24 13:51:04 -0600

    remove messenger

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -12,13 +12,11 @@ import {
 } from "react-share";
 
 const ShareButtons = ({url}) => {
-  return <div className='share-buttons'>
-    
+  return <div className='share-buttons'> 
     <EmailShareButton  url={url}><EmailIcon size={30} borderRadius={10} /></EmailShareButton>
     <FacebookShareButton url={url}><FacebookIcon size={30} borderRadius={10} /></FacebookShareButton>
     <TwitterShareButton url={url}><TwitterIcon size={30} borderRadius={10} /></TwitterShareButton>
     <WhatsappShareButton url={url}><WhatsappIcon size={30} borderRadius={10} /></WhatsappShareButton>
-    <FacebookMessengerShareButton url={url}><FacebookMessengerIcon size={30} borderRadius={10}  /></FacebookMessengerShareButton>
   </div>
 }
 
