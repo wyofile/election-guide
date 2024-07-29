ab62fe87955964674f9a199a8896863d4c1e14f6
Tom Musselman <tmusselman@gmail.com>
2024-07-29 15:55:22 -0600

    change favicon setup for google search

--- src/design/Layout.js
+++ src/design/Layout.js
@@ -45,10 +45,10 @@ const Layout = ({
         <meta name="twitter:description" content={socialDescription} />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
 
-        <link rel="shortcut icon" href="/favicon.ico" />
+        {/* <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/> */}
 
       </Head>
       <Header />
