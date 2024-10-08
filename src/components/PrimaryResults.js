9c239dc0ccd3fd6f81905498cd44c8a04e575315
Tom Musselman <tmusselman@gmail.com>
2024-10-08 12:02:02 -0600

    progress

--- src/components/PrimaryResults.js
+++ src/components/PrimaryResults.js
@@ -62,47 +62,53 @@ const style = css`
 `
 
 const PrimaryResults = ({results}) => {
-    const primaryPartyLabel = PARTIES.find(d => d.key === results.party).adjective
-
-    return <div className="primary-results">
-        <div className="title">August 20 Primary{` – ${primaryPartyLabel} candidates`}</div>
-        <table>
-            <thead>
-                <tr className="result-row">
-                    <th className="result-row-name">Candidate</th>
-                    <th className="result-row-percent">Votes</th>
-                    <th className="result-row-bar">Percentage</th>
-                </tr>
-            </thead>
-            <tbody>{
-                results.candidates
-                    .sort((a, b) => b.votes - a.votes)
-                    .map((c, i) => <Row key={String(i)} {...c} />)
-            }</tbody>
-        </table>
-        <div className="date">Count reported by Montana secretary of state as of {formatDate(new Date(timestamp))}.</div>
-    </div>
+  return (
+    <div>{results.district}</div>
+  )
 }
+
+// const PrimaryResults = ({results}) => {
+//     const primaryPartyLabel = PARTIES.find(d => d.key === results.party).adjective
+
+//     return <div className="primary-results">
+//         <div className="title">August 20 Primary{` – ${primaryPartyLabel} candidates`}</div>
+//         <table>
+//             <thead>
+//                 <tr className="result-row">
+//                     <th className="result-row-name">Candidate</th>
+//                     <th className="result-row-percent">Votes</th>
+//                     <th className="result-row-bar">Percentage</th>
+//                 </tr>
+//             </thead>
+//             <tbody>{
+//                 results.candidates
+//                     .sort((a, b) => b.votes - a.votes)
+//                     .map((c, i) => <Row key={String(i)} {...c} />)
+//             }</tbody>
+//         </table>
+//         <div className="date">Count reported by Montana secretary of state as of {formatDate(new Date(timestamp))}.</div>
+//     </div>
+// }
 
 export default PrimaryResults
 
-const BAR_RANGE = 60
-const Row = ({ candidate, votes, votePercent, isWinner, party }) => {
-    const partyInfo = PARTIES.find(d => d.key === party)
-    const barWidth = votePercent * BAR_RANGE
-    return <tr className="result-row" style={{
-        backgroundColor: isWinner ? 'var(--gray1)' : 'none',
-        borderLeft: `5px solid ${partyInfo.color}`,
-        fontWeight: isWinner ? 'bold' : 'normal',
-    }}>
-        <td className="result-row-name">
-            {isWinner ? <span className="winner-icon" style={{ backgroundColor: partyInfo.color }}>✓</span> : ''}
-            {candidate}
-        </td>
-        <td className="result-row-percent">{numberFormat(votes)}</td>
-        <td className="result-row-bar"><svg width={BAR_RANGE + 50} height={14}>
-            <rect fill={partyInfo.color} x={0} y={0} height={18} width={barWidth} />
-            <text x={barWidth + 5} y={12}>{percentFormat(votePercent)}</text>
-        </svg></td>
-    </tr>
-}
+// const BAR_RANGE = 60
+// const Row = ({ candidate, votes, votePercent, isWinner, party }) => {
+//     const partyInfo = PARTIES.find(d => d.key === party)
+//     const barWidth = votePercent * BAR_RANGE
+//     return <tr className="result-row" style={{
+//         backgroundColor: isWinner ? 'var(--gray1)' : 'none',
+//         borderLeft: `5px solid ${partyInfo.color}`,
+//         fontWeight: isWinner ? 'bold' : 'normal',
+//     }}>
+//         <td className="result-row-name">
+//             {isWinner ? <span className="winner-icon" style={{ backgroundColor: partyInfo.color }}>✓</span> : ''}
+//             {candidate}
+//         </td>
+//         <td className="result-row-percent">{numberFormat(votes)}</td>
+//         <td className="result-row-bar"><svg width={BAR_RANGE + 50} height={14}>
+//             <rect fill={partyInfo.color} x={0} y={0} height={18} width={barWidth} />
+//             <text x={barWidth + 5} y={12}>{percentFormat(votePercent)}</text>
+//         </svg></td>
+//     </tr>
+// }
