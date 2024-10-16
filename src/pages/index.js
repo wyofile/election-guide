e3607da7b0cef803a2d33ecfd0f34f25365e0a7e
Thomas Musselman <tmusselman@gmail.com>
2024-10-15 19:35:29 -0600

    separate supreme court justices

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,36 +88,42 @@ const Home = ({candidates, textContent}) => {
       <h2 className='section-header'>Judge Retention</h2>
       <MarkdownExternalLinks>{textContent.judgeRetentionIntro}</MarkdownExternalLinks>
       <div className='faq-question'>
+        <div className='sup-judges'>
+          <div className='sup-judge-card'>
+            <img className='jd-img' src={usePath(`/judicial-districts/generic.svg`)} alt='Statewide Map'/>
+            <h3 className='jd-title'>Statewide</h3>
+            <h4 className='court-title'>Supreme Court Justices</h4>
+            <ul className='sup-judge-list'>
+              {textContent.judgeContentNew.supremeCourt.judges.map(judge => {
+                return (<li className='sup-judge'><a href={judge.link}>Justice {judge.name} <img src={usePath('/external.svg')}/></a></li>)
+              })}
+            </ul>
+          </div>
+        </div>
         <div className='judge-districts'>
-        {textContent.judgeContentNew.districts.map((district,i) => {
-            return <div key={`d-${i}`} className='judge-district-card'>
-              <img className='jd-img' src={usePath(`/judicial-districts/d-${(i+1)}.svg`)} alt={`${district.title} Map`}/>
-              <h3 className='jd-title'>{district.title}</h3>
-              <h4 className='jd-counties'>{district.counties}</h4>
-              <h4 className='court-title'>Supreme Court Justices</h4>
-              <ul className='sup-judge-list'>
-                {textContent.judgeContentNew.supremeCourt.judges.map(judge => {
-                  return (<li className='sup-judge'><a href={judge.link}>Justice {judge.name} <img src={usePath('/external.svg')}/></a></li>)
-                })}
-              </ul>
-              {district.districtJudges.length> 0 && <>
-                <h4 className='court-title'>District Judges</h4>
-                <ul className='sup-judge-list'>
-                  {district.districtJudges.map((judge, i) => {
-                    return <li key={`judge-${i}`} className='sup-judge'><a href={judge.link}>{judge.name} <img src={usePath('/external.svg')}/></a></li>
-                  })}
-                </ul>
-              </>}
-              {district.circuitJudges.length > 0 && <>
-                <h4 className='court-title'>Circuit Judges</h4>
-                <ul className='sup-judge-list'>
-                  {district.circuitJudges.map((judge, i) => {
-                    return <li key={`judge-${i}`}className='sup-judge'><a href={judge.link}>{judge.name} <img src={usePath('/external.svg')}/></a></li>
-                  })}
-                </ul>
-              </>}
-            </div>
-          })}
+          {textContent.judgeContentNew.districts.map((district,i) => {
+              return <div key={`d-${i}`} className='judge-district-card'>
+                <img className='jd-img' src={usePath(`/judicial-districts/d-${(i+1)}.svg`)} alt={`${district.title} Map`}/>
+                <h3 className='jd-title'>{district.title}</h3>
+                <h4 className='jd-counties'>{district.counties}</h4>
+                {district.districtJudges.length> 0 && <>
+                  <h4 className='court-title'>District Judges</h4>
+                  <ul className='sup-judge-list'>
+                    {district.districtJudges.map((judge, i) => {
+                      return <li key={`judge-${i}`} className='sup-judge'><a href={judge.link}>{judge.name} <img src={usePath('/external.svg')}/></a></li>
+                    })}
+                  </ul>
+                </>}
+                {district.circuitJudges.length > 0 && <>
+                  <h4 className='court-title'>Circuit Judges</h4>
+                  <ul className='sup-judge-list'>
+                    {district.circuitJudges.map((judge, i) => {
+                      return <li key={`judge-${i}`}className='sup-judge'><a href={judge.link}>Judge {judge.name} <img src={usePath('/external.svg')}/></a></li>
+                    })}
+                  </ul>
+                </>}
+              </div>
+            })}
         </div>
       </div>
     </section>
