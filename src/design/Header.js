cd20ed3941e4e7a0d4753611a221b52fdbedfaf9
Thomas Musselman <tmusselman@gmail.com>
2024-11-12 22:41:35 -0700

    bluesky social sharing

--- src/design/Header.js
+++ src/design/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
       <p className='share-label'>Share:</p>
       <div className="a2a_kit a2a_kit_size_32 a2a_default_style" data-a2a-url="https://projects.wyofile.com/election-guide-2024">
         <a className="a2a_button_facebook"></a>
-        <a className="a2a_button_x"></a>
+        <a className="a2a_button_bluesky"></a>
         <a className="a2a_button_whatsapp"></a>
         <a className="a2a_button_facebook_messenger"></a>
         <a className="a2a_button_email"></a>
